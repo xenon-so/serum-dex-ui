ae9908cb199b3d85d993b4b25e53aabeb55f18ef
raashidjunaid1 <raashidjunaid1@gmail.com>
2021-11-25 16:15:02 +0530

    base labels

--- src/xenonCustomMarket.js
+++ src/xenonCustomMarket.js
@@ -6,17 +6,23 @@ export const xenonMarkets = [
     deprecated: false,
     name: 'BTC/USDC',
     programId: new PublicKey('DESVgJVGajEgKGXhb6XmqDHGz3VjdgP7rEVESBgxmroY'),
+    quoteLabel: 'USDC',
+    baseLabel: 'BTC',
   },
   {
     address: new PublicKey('BkAraCyL9TTLbeMY3L1VWrPcv32DvSi5QDDQjik1J6Ac'),
     deprecated: false,
     name: 'ETH/USDC',
     programId: new PublicKey('DESVgJVGajEgKGXhb6XmqDHGz3VjdgP7rEVESBgxmroY'),
+    quoteLabel: 'USDC',
+    baseLabel: 'ETH',
   },
   {
     address: new PublicKey('249LDNPLLL29nRq8kjBTg9hKdXMcZf4vK2UvxszZYcuZ'),
     deprecated: false,
     name: 'SRM/USDC',
     programId: new PublicKey('DESVgJVGajEgKGXhb6XmqDHGz3VjdgP7rEVESBgxmroY'),
+    quoteLabel: 'USDC',
+    baseLabel: 'SRM',
   },
 ];
