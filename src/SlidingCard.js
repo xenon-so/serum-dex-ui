fdc34e83522e4d443646b762b803a246f82f9ce7
raashidjunaid1 <raashidjunaid1@gmail.com>
2021-12-06 20:27:08 +0530

    layouts

--- src/SlidingCard.js
+++ src/SlidingCard.js
@@ -1,16 +1,130 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Progress, Collapse } from 'antd';
 import { CaretDownOutlined } from '@ant-design/icons';
+import {
+  MAX_RATE,
+  OPTIMAL_RATE,
+  OPTIMAL_UTIL,
+  programId,
+} from './utils/constants';
+import { PublicKey } from '@solana/web3.js';
+import { useWallet } from './utils/wallet';
+import { useConnection } from './utils/connection';
+import { MARGIN_DATA, XENON_DATA } from './xenonLayouts';
+
 const { Panel } = Collapse;
 
 const SlidingCard = () => {
   const [state, setState] = useState(false);
+  // const [xenonProgramData, setXenonProgramData] = useState(null);
+  // const [accountMarginData, setAccountMarginData] = useState(null);
+  // const [timer, setTimer] = useState(0);
+  // const [assetValue, setAssetValue] = useState(0);
+  // const [healthPercentage, setHealthPercentage] = useState(0);
+  // const [liabilites, setLiabilites] = useState(0);
+  // const [usdcLeftToBorrow, setUsdcLeftToBorrow] = useState(0);
+  // const [accountLeverage, setAccountLeverage] = useState(0);
+  // const [borrowAPR, setBorrowAPR] = useState(0);
+
+  // const { connected, wallet } = useWallet();
+  // const connection = useConnection();
+
+  // useEffect(() => {
+  //     const interval = setInterval(() => {
+  //         setTimer(Date.now())
+  //     }, 60000);
+  //     return () => clearInterval(interval);
+  // }, []);
+
+  // const subscribeXenonProgramDataSubscribe = async () => {
+  //     console.log(">>>>>>>>>>>>>>>> ")
+  //     const xenonPDA = await PublicKey.findProgramAddress([Buffer.from("xenon_v1")], programId);
+  //     let xenonInfo = await connection.getAccountInfo(xenonPDA[0])
+  //     if (xenonInfo) {
+  //     console.log(">>>>>>>>>>>>>>>> 2222")
+  //         let xenonData = XENON_DATA.decode(xenonInfo.data)
+  //         setXenonProgramData(xenonData);
+
+  //         const xenonProgramDataSubscription = connection.onAccountChange(xenonPDA[0], (x, y) => {
+  //             const data = XENON_DATA.decode(x.data)
+  //             setXenonProgramData(data)
+  //         }, 'recent')
+  //     }
+  // }
+
+  // useEffect(() => {
+  //     subscribeXenonProgramDataSubscribe();
+  // }, [])
+
+  // const subscribeAccountMarginData = async () => {
+  //     if (wallet) {
+  //         const marginPDA = await PublicKey.findProgramAddress([wallet?.publicKey.toBuffer()], programId);
+  //         let xenonInfo = await connection.getAccountInfo(marginPDA[0])
+  //         if (xenonInfo) {
+  //             let xenonData = MARGIN_DATA.decode(xenonInfo.data)
+  //             setAccountMarginData(xenonData)
+
+  //             const solAccountSubscription = connection.onAccountChange(marginPDA[0], (x, y) => {
+  //                 const data = MARGIN_DATA.decode(x.data)
+  //                 setAccountMarginData(data)
+  //             }, 'recent')
+  //         }
+  //     }
+  // }
+
+  // useEffect(() => {
+  //     subscribeAccountMarginData();
+  // }, [wallet])
+
+  // useEffect(() => {
+  //     if (accountMarginData && xenonProgramData && borrowRate) {
+
+  //         const assets = (accountMarginData.assets / 10 ** 6).toFixed(2);
+  //         const currentDate = Math.floor(Date.now() / 1000)
+  //         const lastUpdatedTime = xenonProgramData.last_updated.toNumber();
+  //         const borrowIndex = xenonProgramData.borrow_index + xenonProgramData.borrow_index * borrowRate * (currentDate - lastUpdatedTime)
+  //         const liabs = (accountMarginData.liabs / 10 ** 6) * borrowIndex
+  //         setLiabilites(liabs);
+  //         setUsdcLeftToBorrow(assets - (2 * liabs) < 0 ? 0 : assets - (2 * liabs));
+  //         const coll_ratio = (accountMarginData.assets / 10 ** 6) / liabs;
+  //         setAccountLeverage((1 / (coll_ratio - 1) + 1).toFixed(2))
+  //         setBorrowAPR((borrowRate * 3.154e+7 * 100).toFixed(2));
+  //         setAssetValue((accountMarginData.assets / 10 ** 6).toFixed(2));
+  //         setHealthPercentage((((accountMarginData.assets / accountMarginData.liabs) - 1.2) * 100).toFixed(0));
+  //     }
+  // }, [accountMarginData, xenonProgramData, borrowRate])
+
+  // const borrowRate = useMemo(() => {
+  //     if (xenonProgramData) {
+  //         const totalBorrows = xenonProgramData.total_borrows
+  //         const totalDeposits = xenonProgramData.total_deposits
+
+  //         if (totalDeposits === 0 && totalBorrows === 0) {
+  //             return 0
+  //         }
+  //         if (totalDeposits <= totalBorrows) {
+  //             return MAX_RATE
+  //         }
+
+  //         const utilization = totalBorrows / totalDeposits
+  //         if (utilization > OPTIMAL_UTIL) {
+  //             const extraUtil = utilization - OPTIMAL_UTIL
+  //             const slope = (MAX_RATE - OPTIMAL_RATE) / (1 - OPTIMAL_UTIL)
+  //             return OPTIMAL_RATE + slope * extraUtil
+  //         } else {
+  //             const slope = OPTIMAL_RATE / OPTIMAL_UTIL
+  //             return slope * utilization
+  //         }
+  //     } else {
+  //         return 0
+  //     }
+  // }, [xenonProgramData, timer])
+
   return (
     <div className={`sliding_card_warper ${state ? `active_card` : ``}`}>
       <div
         className="sliding_card_switch"
         onClick={() => {
-          console.log('heee');
           setState(!state);
         }}
       >
@@ -142,7 +256,7 @@ const SlidingCard = () => {
             </table>
           </Panel>
         </Collapse>
-        <button className="sliding-card-btn">Get 200$ Faucet</button>
+        <button className="sliding-card-btn">Get Devnet USDC </button>
       </div>
     </div>
   );
---
+++ src/utils/constants.ts
@@ -0,0 +1,23 @@
+import { Connection, PublicKey } from "@solana/web3.js";
+
+export const cluster = "https://api.devnet.solana.com";
+export const connection = new Connection(cluster, "confirmed");
+
+export const programId = new PublicKey('CLbDtJTcL7NMtsujFRuHx5kLxjDgjmEuM2jZqswk7bbN');
+
+export const SYSTEM_PROGRAM_ID = new PublicKey('11111111111111111111111111111111')
+export const TOKEN_PROGRAM_ID = new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA')
+export const MEMO_PROGRAM_ID = new PublicKey('Memo1UhkJRfHyvLMcVucJwxXeuD728EqVDDwQDxFMNo')
+export const RENT_PROGRAM_ID = new PublicKey('SysvarRent111111111111111111111111111111111')
+export const CLOCK_PROGRAM_ID = new PublicKey('SysvarC1ock11111111111111111111111111111111')
+export const ASSOCIATED_TOKEN_PROGRAM_ID = new PublicKey('ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL')
+
+// devnet faucet data
+export const FAUCET_PROGRAM_ID = new PublicKey('4bXpkKSV8swHSnwqtzuboGPaPDeEgAn4Vt8GfarV5rZt')
+export const FAUCET_KEY = new PublicKey('B87AhxX6BkBsj3hnyHzcerX2WxPoACC7ZyDr8E7H9geN')
+
+export const MAX_RATE = 4.75 * 10**-8;
+export const MAX_RATE_YEAR = 1.5
+export const OPTIMAL_UTIL = 0.7
+export const OPTIMAL_RATE = 1.90 * 10**-9;
+export const OPTIMAL_RATE_YEAR = 0.06
